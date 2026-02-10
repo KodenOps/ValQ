e8393b275e1ac21b38d5d8945513ada2cc350774
kodenops <segun.femifadiya@gmail.com>
2026-02-10 15:12:41 +0100

    Update title in metadata for consistency

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,7 +13,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-	title: 'ValQ-Morenikeji',
+	title: 'Morenikeji',
 	description: 'Specially created for Morenikeji',
 };
 
